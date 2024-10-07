4f8f6e6c06fde800b5777301c7b153ddcea3c575
ravi1729-singh <rs5700166@gmail.com>
2024-10-07 09:55:36 +0530

    project completed

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -1,3 +1,41 @@
+// const passport = require("passport");
+// const GoogleStrategy = require("passport-google-oauth2").Strategy;
+// const User = require("../routes/registerModels");
+
+// //passport google strategy
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID:
+//         "195118986970-6sr88jhohv2hm9c6b484gpci44ocook7.apps.googleusercontent.com",
+//       clientSecret: "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",
+//       callbackURL: "https://blog-site-by-vihan.herokuapp.com/google/callback",
+//       passReqToCallback: true,
+//     },
+//     function (request, accessToken, refreshToken, profile, done) {
+//       User.findOrCreate(
+//         {
+//           name: profile.displayName,
+//           email: profile.email,
+//           provider: "GOOGLE",
+//         },
+//         function (err, user) {
+//           return done(err, user);
+//         }
+//       );
+//     }
+//   )
+// );
+
+// passport.serializeUser(function (user, done) {
+//   done(null, user);
+// });
+
+// passport.deserializeUser(function (user, done) {
+//   done(null, user);
+// });
+
+
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const User = require("../routes/registerModels");
@@ -6,12 +44,9 @@ const User = require("../routes/registerModels");
 passport.use(
   new GoogleStrategy(
     {
-      clientID: "",
-      // "195118986970-6sr88jhohv2hm9c6b484gpci44ocook7.apps.googleusercontent.com",
-      // clientSecret: "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",
-      clientSecret: "",
-      callbackURL: "",
-      // "https://blog-site-by-vihan.herokuapp.com/google/callback",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL, // You should also store the callback URL in the environment variable
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
